9578fd6f24cace54f1dcb5120643454296c88f5b
adrioro <artistsvillage@me.com>
2019-01-24 17:07:54 -0500

    Second commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+$("h1").click(function () {
+    $(this).slideUp();
+
+});
+*/
+//.slideUp
+$("p").click(function () {
+    $(this).slideUp();
+});
+
+$("#buttonId").click(function () {
+    $("h3.thirdId").slideUp();
+})
+
+//.append
+$("#buttId").click(function () {
+    $("button").after("<br><br><b>Hello</b>");
+})
